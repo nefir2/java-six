37f7ed8d9e30c1cf5f5ee430b89fb1f00266283f
nefir2 <vadim.ahtamov@gmail.com>
2022-11-12 00:38:47 +0300

    tryed make Course<T>.
    this is impossible...

--- courses/src/Course.java
+++ courses/src/Course.java
@@ -1,11 +1,6 @@
-//import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.*;
 public class Course {
-    private String courseName;
+	private String courseName;
     private String[] students;
     private int numberOfStudents;
 
@@ -16,14 +11,40 @@ public class Course {
     public Course(String courseName) {
         numberOfStudents = 0;
         this.courseName = courseName;
+		students = new String[0];
     }
 
     public void addStudent(String student) {
         numberOfStudents++;
+		ArrayList<String> studs = new ArrayList<String>(Arrays.asList(students));
+		studs.add(student);
         students = new String[numberOfStudents];
-        List<String> studs = new ArrayList<String>(Arrays.asList(students));
-        studs.add(student);
-        students = studs.toArray();
+        for (int i = 0; i < numberOfStudents; i++) students[i] = studs.get(i);
     }
-    public void dropStudent(String student) { }
+    public void dropStudent(String student) {
+		numberOfStudents--;
+		ArrayList<String> studs = new ArrayList<String>(Arrays.asList(students));
+		studs.remove(student);
+		students = new String[numberOfStudents];
+		for (int i = 0; i < numberOfStudents; i++) students[i] = studs.get(i);
+	}
+
+	public void addStudent(String... students) { for (String student : students) addStudent(student); }
+	public void dropStudent(String... students) { for (String student : students) dropStudent(student); }
+
+	public void clear() {
+		students = new String[0];
+		numberOfStudents = 0;
+	}
+
+	//вывод элементов массива, и установка запятых, после каждого элемента. в конце массива устанавливается точка.
+	public void showStudents() { for (int i = 0; i < numberOfStudents; i++) System.out.print(students[i] + (i == numberOfStudents - 1 ? ".\n" : ", ")); }
+
+	@Override
+	public String toString() {
+		String ret = "Количество студентов по дисциплине " + courseName + ": " + numberOfStudents;
+		ret = ret.concat("\nстуденты на курсе: ");
+		for (int i = 0; i < numberOfStudents; i++) ret = ret.concat(students[i] + (i == numberOfStudents - 1 ? ".\n" : ", "));
+		return ret;
+	}
 }
--- courses/src/Main.java
+++ courses/src/Main.java
@@ -1,19 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Course course1 = new Course("\"основы программирования\"");
-        Course course2 = new Course("\"объектно-ориентированное программирование\"");
-
-        course1.addStudent("Петр Иванович Н.");
-        course1.addStudent("Борис Сергеевич П.");
-        course1.addStudent("Анна Дмитриевна К.");
-        course2.addStudent("Петр Иванович Н.");
-        course2.addStudent("Анна Сергеевна Д.");
-        System.out.printf("Количество студентов по дисциплине\n%s: %d\n", course1.getCourseName(), course1.getNumberOfStudents());
-        String[] students = course1.getStudents();
-        for (int i = 0; i < course1.getNumberOfStudents(); i++)
-            System.out.print(students[i] + ", ");
-
-        System.out.println();
-        System.out.print("Количество студентов по дисциплине\n" + course2.getCourseName() + ": " + course2.getNumberOfStudents());
+        Course course = new Course("\"основы программирования\"");
+        course.addStudent("какой-та нн", "какой-та второй нн", "очередной нн");
+        course.dropStudent("какой-та второй нн");
+        System.out.println(course);
     }
 }
