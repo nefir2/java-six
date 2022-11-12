3b34040f1e067462f313bb49717dec1dccbb386c
nefir2 <vadim.ahtamov@gmail.com>
2022-11-12 17:50:06 +0300

    no more generics in main
    and all tabs, again

--- courses/src/Main.java
+++ courses/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        Course course = new Course("\"основы программирования\"");
-        course.addStudent("какой-та нн", "какой-та второй нн", "очередной нн");
-        course.dropStudent("какой-та второй нн");
-        System.out.println(course);
-    }
+	public static void main(String[] args) {
+		Course course = new Course("\"основы программирования\"");
+		course.addStudent("какой-та нн", "какой-та второй нн", "очередной нн");
+		course.dropStudent("какой-та второй нн");
+		System.out.println(course);
+	}
 }
