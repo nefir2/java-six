83b3eba853a2e4fe6c4b81bb9b99475fe6a30bd3
nefir2 <vadim.ahtamov@gmail.com>
2022-11-12 17:48:15 +0300

    all tabs

--- courses/src/Main.java
+++ courses/src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        Class x = new Student().getClass();
-        Course<Student> course = new Course<Student>("\"основы программирования\"", x);
+	public static void main(String[] args) {
+		Class x = new Student().getClass();
+		Course<Student> course = new Course<Student>("\"основы программирования\"", x);
 
-        Student first = new Student("какой-та", "нн", "16", 3);
-        Student second = new Student("нн", null, null, null);
-        Student third = new Student("очередной", "нн", "135", 1);
+		Student first = new Student("какой-та", "нн", "16", 3);
+		Student second = new Student("нн", null, null, null);
+		Student third = new Student("очередной", "нн", "135", 1);
 
-        course.addStudent(first, second, third); //"какой-та нн", "какой-та второй нн", "очередной нн"
+		course.addStudent(first, second, third); //"какой-та нн", "какой-та второй нн", "очередной нн"
 
-        course.dropStudent(); //"какой-та второй нн"
+		course.dropStudent(); //"какой-та второй нн"
 
-        System.out.println(course);
-    }
+		System.out.println(course);
+	}
 }
